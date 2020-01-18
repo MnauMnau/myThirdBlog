cbf6b4985969c69348b719e6a94e5a8d66a865d3
MnauMnau <skoty951@gmail.com>
2020-01-18 23:48:26 +0100

    Added summernote functionality to simlif text editing

--- myThirdBlog/blog/blog/settings.py
+++ myThirdBlog/blog/blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# This was added due to summernote engine
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+# I do not know if it should be here, its from summernote installation notes
+X_FRAME_OPTIONS = 'SAMEORIGIN'
--- myThirdBlog/blog/entries/admin.py
+++ myThirdBlog/blog/entries/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 from .models import Entry
 
-admin.site.register(Entry)
+class SomeModelAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+
+admin.site.register(Entry,SomeModelAdmin)
