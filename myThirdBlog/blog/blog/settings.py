5addd76f65e2f00b71cf6e485dba1f73bcf7f091
MnauMnau <skoty951@gmail.com>
2020-01-19 19:18:11 +0100

    added static files for adminpanel

--- myThirdBlog/blog/blog/settings.py
+++ myThirdBlog/blog/blog/settings.py
@@ -15,6 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Getting location of admin directory
+ADMIN_DIR = BASE_DIR.rsplit('/',1)
+ADMIN_DIR = os.path.join(ADMIN_DIR[0],'venv')
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -120,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(ADMIN_DIR,'lib64/python3.6/site-packages/django/contrib/admin/static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'entries/static')
 
 # This was added due to summernote engine
 MEDIA_URL = '/media/'
