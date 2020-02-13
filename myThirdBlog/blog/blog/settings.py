02528e12b0b9185d0ac988649af4e9e0b3f49291
MnauMnau <skoty951@gmail.com>
2020-02-13 21:53:55 +0100

    Trying to add .gitignore file

--- myThirdBlog/blog/blog/settings.py
+++ myThirdBlog/blog/blog/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.sites',
     'django.contrib.sitemaps',
-    'imagesitemaps',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
