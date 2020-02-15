65089b67966c12a0bca498b9afaa24f1b8513858
MnauMnau <skoty951@gmail.com>
2020-02-15 21:46:52 +0100

    Imported image sitemap

--- myThirdBlog/blog/entries/urls.py
+++ myThirdBlog/blog/entries/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import HomeView, EntryView, CreateEntryView, AboutMeView
 from django.contrib.sitemaps.views import sitemap # This need to be included because of sitemaps
+from django.views.generic import TemplateView
 
 from .sitemaps import EntrySitemap
 
@@ -19,7 +20,8 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps' : sitemaps}), # new url for sitemaps
     path('entry/<int:pk>',EntryView.as_view(), name = 'entry-detail' ),
     path('create_entry',CreateEntryView.as_view(success_url = '/'), name = 'create_entry'),
-    path('about_me',AboutMeView.as_view() ,name = 'about_me')
+    path('about_me',AboutMeView.as_view() ,name = 'about_me'),
+    path('image-sitemap.xml',TemplateView.as_view(template_name='entries/image-sitemap.xml', content_type='text/xml'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
